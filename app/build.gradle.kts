e2804c194811aca45ff76b986cce15f854310ae6
saboorindus <saboorindus@gmail.com>
2026-04-29 13:15:12 +0500

    Release version 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,10 +21,6 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
-
-        ndk {
-            abiFilters += listOf("arm64-v8a")
-        }
     }
 
     applicationVariants.all {
