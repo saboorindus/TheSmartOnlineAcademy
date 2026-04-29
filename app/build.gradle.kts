6d8a6a9e2b9fe2c80ada8b56773a1c113df3769d
saboorindus <saboorindus@gmail.com>
2026-04-29 14:37:58 +0500

    Release version 1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.echologics.thesmartonlineacademy"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
